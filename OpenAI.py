0856cc06a9d3bac417fccb589a01d08a46e0c636
tsuth2 <tisut@schools.vic.edu.au>
2024-10-03 11:32:57 +1000

    3rd commit

--- OpenAI.py
+++ OpenAI.py
@@ -1,15 +1,21 @@
 from openai import OpenAI
 import os
 
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+def api_response(transcription):
+  client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are speaking to somebody on the phone that is discussing computer issues, and are purposely being unhelpful to them because they are a scammer."},
+      {"role": "user", "content": [{"type": "text", "text": f"{transcription}"}]} #"Compose a poem that explains the concept of recursion in programming."}
+    ]
+  )
+
+  return completion
 
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
-
-print(completion.choices[0].message)
+if __name__ == "__main__":
+    transcription = "Sigma!"
+    completion = api_response(transcription)
+    print(completion.choices[0].message)
